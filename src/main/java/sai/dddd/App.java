80960f8654bbebe5c46ca550c54b5a4c893dd5a1
sai <gajularamavenkata@gmail.com>
2023-11-17 16:46:49 +0530

    wsert

---
+++ src/main/java/sai/dddd/App.java
@@ -0,0 +1,13 @@
+package sai.dddd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
